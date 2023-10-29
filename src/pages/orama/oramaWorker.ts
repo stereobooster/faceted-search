5dd94744527cdda6e4fd9ab7d60d1f949dcae4ab
stereobooster <stereobooster@gmail.com>
2023-10-29 16:29:16 +0100

    maybe

--- src/pages/orama/oramaWorker.ts
+++ src/pages/orama/oramaWorker.ts
@@ -14,12 +14,12 @@ import {
   create,
   insertMultiple,
   search,
-  Orama
+  Orama,
 } from "@orama/orama";
 
 // Module format "iife" does not support top-level await. Use the "es" or "system" output formats rather.
 let ecomerceDB: Orama<typeof oramaSchema>;
-create({ schema: oramaSchema }).then((x) => {
+const ecomerceDBPromise = create({ schema: oramaSchema }).then((x) => {
   ecomerceDB = x;
 });
 
@@ -36,20 +36,22 @@ const load = (limit = defaultLimit) => {
   if (loading > 0) return false;
   loading = 1;
 
-  loadData(limit).then(async (data) => {
-    let tmp = [];
-    for (let i = 0; i < limit; i++) {
-      tmp.push(data[i]);
-      if (tmp.length >= batch || i === limit - 1) {
-        // @ts-expect-error Orama TS signature is wrong
-        await insertMultiple(ecomerceDB, tmp);
-        total += tmp.length;
-        tmp = [];
-        if (callback) callback(i / (limit - 1), total);
-        // await pause(50);
+  ecomerceDBPromise.then(() => {
+    loadData(limit).then(async (data) => {
+      let tmp = [];
+      for (let i = 0; i < limit; i++) {
+        tmp.push(data[i]);
+        if (tmp.length >= batch || i === limit - 1) {
+          // @ts-expect-error Orama TS signature is wrong
+          await insertMultiple(ecomerceDB, tmp);
+          total += tmp.length;
+          tmp = [];
+          if (callback) callback(i / (limit - 1), total);
+          // await pause(50);
+        }
       }
-    }
-    loading = 2;
+      loading = 2;
+    });
   });
 
   return true;
@@ -74,13 +76,15 @@ const api = {
     callback = cb;
   },
   search({ signalId, ...rest }: SearchParamsOramaWorker) {
-    return search(ecomerceDB, {
-      ...rest,
-      properties: searchFields,
-      facets: oramaFacets,
-    }).then((res: ResultsOramaWorker) => {
-      res.signalId = signalId;
-      return res;
+    return ecomerceDBPromise.then(() => {
+      return search(ecomerceDB, {
+        ...rest,
+        properties: searchFields,
+        facets: oramaFacets,
+      }).then((res: ResultsOramaWorker) => {
+        res.signalId = signalId;
+        return res;
+      });
     });
   },
 };
